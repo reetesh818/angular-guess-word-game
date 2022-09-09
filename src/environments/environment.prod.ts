ef1cab19a67838f4384344becfc15263334d49bb
reetesh818 <reet.ug19cse18@opju.ac.in>
2022-09-09 11:56:45 +0530

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api_host: "https://fourtytwowords.herokuapp.com",
+  api_key: "be45adfee7c617ff1b22a4ffccdf2687a8b7f484d1fc0603388c9f5d51879871e6fa92b0cb6fa6915f86e5c59d2c815b45496db11041a065ff6339318c925201",
+  
 };
